47941051b30865438b92d4e2f7b6f6309c83659b
Thien Nguyen <spacemannormal@gmail.com>
2026-06-12 11:55:13 +0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let smallest = prices[0]
+
+        for (let left = 0; left < prices.length; left++) {
+            if (prices[left] > smallest) {
+                continue;
+            }
+
+            console.log(left);
+
+            for (let right = left + 1; right < prices.length; right++) {
+                if (prices[right] > prices[left]) {
+                    if (prices[right] - prices[left] > maxProfit) {
+                        maxProfit = prices[right] - prices[left];
+                        smallest = prices[left];
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+
+        return maxProfit;
+    }
+}

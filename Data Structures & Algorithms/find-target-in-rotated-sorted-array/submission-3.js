eaed1128c7f4fcabe633a3265e3715b10c659f25
Thien Nguyen <spacemannormal@gmail.com>
2026-06-15 13:44:02 +0700

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.js
@@ -0,0 +1,76 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+
+        let result = -1;
+
+        function bns(start,end) {
+            if (nums[start] > target) {
+                return;
+            } 
+
+            if(nums[end] < target) {
+                return;
+            }
+
+            while (start <= end) {
+                let middle = Math.floor((start+end) / 2);
+    
+                if (nums[middle] == target) {
+                    return middle;
+                }
+    
+                if (nums[middle] > target) {
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
+                }
+            }
+        }
+
+        while (left <= right) {
+            if (nums[left] <= nums[right]) {
+                const search = bns(left, right);
+
+                if (search >= 0) {
+                    result = search;
+                }
+                break;
+            }
+
+            let middle = Math.floor((left + right) / 2)
+
+            if (nums[middle] == target) {
+                result = middle;
+                break;
+            }
+
+            if (nums[middle] >= nums[left]) {
+                // do bns 
+                const search = bns(left, middle - 1);
+
+                if (search >= 0) {
+                    result = search;
+                }
+
+                left = middle + 1;
+            } else {
+                const search = bns(middle + 1, right);
+
+                if (search >= 0) {
+                    result = search;
+                }
+
+                right = middle - 1;
+            }
+        }
+
+        return result;
+    }
+}
